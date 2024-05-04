45f04850afda510bf10adb84e0c5834ea75c8536
Daviddairot <133882462+Daviddairot@users.noreply.github.com>
2024-05-04 15:38:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import cv2
 
 app = Flask(__name__)
 
+def index():
+    # Render a template with a button to request camera permission
+    return render_template('index.html')
+
 def generate_frames():
     camera = cv2.VideoCapture(0)  # Open the webcam (camera index 0)
     
